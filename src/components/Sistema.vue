f71dee2d0f0b967b713cfc3c8fdcaa11ff11a93f
Facundo Zapata <facundozapata41@gmail.com>
2023-04-19 01:10:45 -0300

    clase 18/4/23 componentes VUE.js

---
+++ src/components/Sistema.vue
@@ -0,0 +1,36 @@
+<template>
+  <div>
+    <h2>Sistema de Gestión</h2>
+    <li v-for="ges in gestiones" :key="ges.id"> <!--lista todas las gestiones disponibles -->
+      {{ ges.id }} {{ ges.desc }}
+    </li>
+    <button v-on:click="OrdenarPorId">OrdenarPorId</button> <!--lista todas las gestiones disponibles -->
+    <Detalle @agregarDetalle="agregarGestion"/>
+  </div>
+</template>
+
+<script>
+import Detalle from './Detalle.vue'
+export default {
+  components: { Detalle},
+  data() {
+    return {
+      gestiones: [{id:10,desc:"Envio mail"},{id:11,desc:"Envio whatsapp"}],
+    }
+  },
+  methods: {
+    agregarGestion(gestion) {
+      this.gestiones.push(gestion);      
+    },
+    OrdenarPorId(){
+      this.gestiones.sort((a,b)=>a.id.localeCompare(b.id))
+    }
+  }
+}
+</script>
+
+
+
+<style>
+
+</style>
